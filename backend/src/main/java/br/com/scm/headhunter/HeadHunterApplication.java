a6a654d3fdc33e8f5ffeb4b2cc3a989b35120a82
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2022-05-15 10:49:54 -0300

    First commit

---
+++ backend/src/main/java/br/com/scm/headhunter/HeadHunterApplication.java
@@ -0,0 +1,13 @@
+package br.com.scm.headhunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadHunterApplication.class, args);
+	}
+
+}
